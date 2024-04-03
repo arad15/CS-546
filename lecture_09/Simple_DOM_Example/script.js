480f01fd16b6b1d7ae82c0e2ea8473388c1145ca
arad15 <79551067+arad15@users.noreply.github.com>
2024-04-03 12:34:04 -0400

    Update script.js

--- lecture_09/Simple_DOM_Example/script.js
+++ lecture_09/Simple_DOM_Example/script.js
@@ -1,25 +1,28 @@
+
 let myForm = document.getElementById('myForm');
 let textInput = document.getElementById('text_input');
 let errorDiv = document.getElementById('error');
 let myUl = document.getElementById('list');
 let frmLabel = document.getElementById('formLabel');
+
 if (myForm) {
+  // sometimes (event) is types as (e)
   myForm.addEventListener('submit', (event) => {
     console.log('Form submission fired');
-    event.preventDefault();
+    event.preventDefault(); // when the form is submitted, the data is prevented from being sent instantly
     console.log('Has a form');
     if (textInput.value.trim()) {
       textInput.classList.remove('inputClass');
       errorDiv.hidden = true;
       frmLabel.classList.remove('error');
-      let li = document.createElement('li');
+      let li = document.createElement('li'); // creates the REFERENCE, doesn't show up yet
 
       li.innerHTML = textInput.value;
-      myUl.appendChild(li);
+      myUl.appendChild(li); // NOW it's displayed 
       myForm.reset();
       textInput.focus();
     } else {
-      textInput.value = '';
+      textInput.value = ''; // only if input is '       ' (remove spaces)
       errorDiv.hidden = false;
       errorDiv.innerHTML = 'You must enter a value';
       frmLabel.className = 'error';
