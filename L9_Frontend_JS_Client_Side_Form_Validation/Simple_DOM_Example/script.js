dbd18c101baf24068502272a35b55b9ac5251d81
arad15 <79551067+arad15@users.noreply.github.com>
2024-04-10 16:14:41 -0400

    Added all lecture comments for the simple DOM example

--- L9_Frontend_JS_Client_Side_Form_Validation/Simple_DOM_Example/script.js
+++ L9_Frontend_JS_Client_Side_Form_Validation/Simple_DOM_Example/script.js
@@ -1,33 +1,35 @@
-
-let myForm = document.getElementById('myForm');
-let textInput = document.getElementById('text_input');
-let errorDiv = document.getElementById('error');
-let myUl = document.getElementById('list');
-let frmLabel = document.getElementById('formLabel');
+let myForm = document.getElementById("myForm"); // get a reference to the form ...
+let textInput = document.getElementById("text_input"); // input
+let errorDiv = document.getElementById("error"); // div
+let myUl = document.getElementById("list"); // ul
+let frmLabel = document.getElementById("formLabel"); // label
 
 if (myForm) {
-  // sometimes (event) is types as (e)
-  myForm.addEventListener('submit', (event) => {
-    console.log('Form submission fired');
-    event.preventDefault(); // when the form is submitted, the data is prevented from being sent instantly
-    console.log('Has a form');
+  // sometimes (event) is typed as (e)
+  // 2 parameters:
+  // 1) the event we're listening for ("submit" event for the button)
+  // 2) function (event or e)
+  myForm.addEventListener("submit", (event) => {
+    console.log("Form submission fired");
+    event.preventDefault(); // prevent the default behavior of form submission; it wants to post somewhere, causing the page to reload
+    console.log("Has a form");
     if (textInput.value.trim()) {
-      textInput.classList.remove('inputClass');
-      errorDiv.hidden = true;
-      frmLabel.classList.remove('error');
-      let li = document.createElement('li'); // creates the REFERENCE, doesn't show up yet
+      textInput.classList.remove("inputClass");
+      errorDiv.hidden = true; // hide the error div, as we don't wanna show a previous error when this input succeeds
+      frmLabel.classList.remove("error");
+      let li = document.createElement("li"); // creates a REFERENCE to an li element, doesn't show up yet
 
-      li.innerHTML = textInput.value;
-      myUl.appendChild(li); // NOW it's displayed 
-      myForm.reset();
-      textInput.focus();
+      li.innerHTML = textInput.value; // give this new element a value
+      myUl.appendChild(li); // NOW it's displayed (once it is added to a parent node (ul) )
+      myForm.reset(); // resets the form
+      textInput.focus(); // focuses the cursor back to the text input
     } else {
-      textInput.value = ''; // only if input is '       ' (remove spaces)
-      errorDiv.hidden = false;
-      errorDiv.innerHTML = 'You must enter a value';
-      frmLabel.className = 'error';
+      textInput.value = ""; // only if input is '       ' (remove spaces)
+      errorDiv.hidden = false; // show the error div
+      errorDiv.innerHTML = "You must enter a value";
+      frmLabel.className = "error";
       textInput.focus();
-      textInput.className = 'inputClass';
+      textInput.className = "inputClass";
     }
   });
 }
--- L9_Frontend_JS_Client_Side_Form_Validation/Simple_DOM_Example/script_jq.js
+++ L9_Frontend_JS_Client_Side_Form_Validation/Simple_DOM_Example/script_jq.js
@@ -1,16 +1,20 @@
+//  $ to reference a field in jquery, targeting an id as we would in css (with #)
+// listens for the submit event
 $('#myForm').submit((event) => {
-  event.preventDefault();
+  event.preventDefault(); // prevent the default behavior of form submission; it wants to post somewhere, causing the page to reload
+  // NOTE: it's val(), not value
   if ($('#text_input').val().trim()) {
     $('#error').hide();
     $('#formLabel').removeClass('error');
     $('#text_input').removeClass('inputClass');
-    const li = `<li class="error" width=> ${$('#text_input').val()} </li>`;
-    $('#list').append(li);
-    $('#myForm').trigger('reset');
-    $('#text_input').focus();
+    // we can fully create this li item
+    const li = `<li width=> ${$('#text_input').val()} </li>`;
+    $('#list').append(li); // add li to the ul
+    $('#myForm').trigger('reset'); // rest the form
+    $('#text_input').focus(); // point cursor back to the text input
   } else {
-    $('#error').show();
-    $('#error').html('You must enter an input value');
+    $('#error').show(); // oppose of hide
+    $('#error').html('You must enter an input value'); // setting the innerHTML
     $('#formLabel').addClass('error');
     $('#text_input').addClass('inputClass');
     $('#text_input').focus();
